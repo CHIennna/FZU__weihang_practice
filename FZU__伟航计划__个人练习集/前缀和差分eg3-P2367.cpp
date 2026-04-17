95d95c155a8d805b0cab182657bd62ab554748e3
CHIennna <chiennna3505@gmail.com>
2026-04-17 18:11:45 +0800

    前缀和和差分到此为止了

---
+++ FZU__伟航计划__个人练习集/前缀和差分eg3-P2367.cpp
@@ -0,0 +1,43 @@
+#include <vector>
+#include <iomanip>
+#include <iostream>
+#include <utility>
+#include <climits>
+#include <algorithm>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<long, long> pll;
+constexpr int maxn = 310;
+constexpr ll INF = 4e18;
+constexpr double eps = 1e-9;
+
+int n, p;
+
+signed main() {
+	//差分模板题
+	cin.tie(0), cout.tie(0);
+	ios::sync_with_stdio(false);
+	cin >> n >> p;
+	vector<int> a(n+1, 0);
+	vector<int> diff(n+1, 0);
+	int Ans = INT_MAX;
+	for(int i = 1; i <= n; i++) {
+		cin >> a[i];
+		diff[i] = a[i] - a[i - 1];
+	}
+	for (int i = 1; i <= p; i++) {
+		int x, y, z;
+		cin >> x >> y >> z;
+		diff[x] += z;
+		diff[y + 1] -= z;
+	}
+	for (int i = 1; i <= n; i++) {
+		diff[i] += diff[i - 1];
+	}//前缀和还原
+	for (int i = 1; i <= n; i++) {
+		Ans = min(Ans, diff[i]);
+	}
+	cout << Ans << '\n';
+	return 0;
+}
