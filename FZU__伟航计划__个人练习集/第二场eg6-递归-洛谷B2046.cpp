b473a6239e9145a5ce807b25b7891b0ecfa8e85d
CHIennna <chiennna3505@gmail.com>
2026-04-25 22:44:21 +0800

    1

--- FZU__伟航计划__个人练习集/第二场eg6-递归-洛谷B2046.cpp
+++ FZU__伟航计划__个人练习集/第二场eg6-递归-洛谷B2046.cpp
@@ -1,3 +1,25 @@
 #include <queue>
 #include <iostream>
 using namespace std;
+
+int fibi(int x) {
+    if (x <= 2) {
+        return 1;
+    }
+    return fibi(x - 1) + fibi(x - 2);
+}
+
+int n;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0), cout.tie(0);
+    cin >> n;
+    vector<int> a(n, 0);
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        cout << fibi(x) << endl;
+    }
+	return 0;
+}
