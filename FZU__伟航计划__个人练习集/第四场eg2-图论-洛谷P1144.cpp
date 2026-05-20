f1af91acbc1e3a5ed6ab9b4c2df2c1e82a1eb388
CHIennna <chiennna3505@gmail.com>
2026-05-20 23:04:03 +0800

    1

--- FZU__伟航计划__个人练习集/第四场eg2-图论-洛谷P1144.cpp
+++ FZU__伟航计划__个人练习集/第四场eg2-图论-洛谷P1144.cpp
@@ -4,28 +4,43 @@
 #include <algorithm>
 using namespace std;
 const int MAXN = 100005;
+const int FUCK = 100003;
 
 vector<int> e[MAXN];
-int cnt[MAXN];
-int dis[MAXN];
+int cnt[MAXN];//1到i的最短路条数
+int dis[MAXN];//1到i的最短距离
 int vis[MAXN];
 
 int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	int n, m;
+	cin >> n >> m;
+	for (int i = 1; i <= m; i++) {
+		int x, y;
+		cin >> x >> y;
+		e[x].push_back(y);
+		e[y].push_back(x); // 无向图，双向建边
+	}
 	queue<int> q;
 	q.push(1);
 	vis[1] = 1, dis[1] = 0, cnt[1] = 1;
 	while (!q.empty()) {
-		int x = q.front(); q.pop();
+		int x = q.front(); 
+		q.pop();
 		for (int y : e[x]) {
 			if (!vis[y]) {
 				dis[y] = dis[x] + 1;
 				cnt[y] = cnt[x];
 				vis[y] = 1;
 				q.push(y);
-			}
-			else if (dis[y] == dis[x] + 1) {
-				cnt[y] = (cnt[y] + cnt[x]) % 100003;
+			}else if (dis[y] == dis[x] + 1) {
+				cnt[y] = (cnt[y] + cnt[x]) % FUCK;
 			}
 		}
 	}
+	for (int i = 1; i <= n; i++) {
+		cout << cnt[i] % FUCK << '\n';
+	}
+	return 0;
 }
