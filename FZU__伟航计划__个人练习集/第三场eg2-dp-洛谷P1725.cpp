f9cbc9a345d45a0766726da44cee8146cc68e75d
CHIennna <chiennna3505@gmail.com>
2026-05-21 23:44:10 +0800

    1

--- FZU__伟航计划__个人练习集/第三场eg2-dp-洛谷P1725.cpp
+++ FZU__伟航计划__个人练习集/第三场eg2-dp-洛谷P1725.cpp
@@ -1,4 +1,5 @@
 #include <vector>
+#include <deque>
 #include <iomanip>
 #include <iostream>
 #include <utility>
@@ -7,16 +8,46 @@ using namespace std;
 typedef long long ll;
 typedef pair<int, int> pii;
 typedef pair<long, long> pll;
-constexpr int maxn = 310;
-constexpr ll INF = 4e18;
+constexpr int maxn = 200005;
+constexpr int INF = 0x3F3F3F3F;
 constexpr double eps = 1e-9;
 
 int n, l, r;
-ll a[maxn] = { 0 };
-
+ll A[maxn] = { 0 };
+ll dp[maxn];
+//P1725 琪露诺
 signed main() {
 	cin.tie(0), cout.tie(0);
 	ios::sync_with_stdio(false);
 	cin >> n >> l >> r;
-
+	for (int i = 0; i <= n; i++) {
+		cin >> A[i];
+	}
+	for (int i = 0; i <= n; i++) {
+		dp[i] = -INF;
+	}
+	dp[0] = A[0];
+	deque<int> q;
+	for (int i = 1; i <= n; i++) {
+		int index = i - l;
+		if (index >= 0) {
+			while (!q.empty() && dp[q.back()] <= dp[index]) {
+				q.pop_back();
+			}
+			q.push_back(index);
+		}
+		while (!q.empty() && q.front() < (i - r)) {
+			q.pop_front();
+		}
+		if (!q.empty()) {
+			dp[i] = A[i] + dp[q.front()];
+		}
+	}
+	ll ans = -INF;
+	//注意这里的范围 不要从零开始 应当是从哪个能够一步跳出到结尾的
+	for (int i = max(0,n-r+1); i <= n; i++) {
+		ans = max(ans, dp[i]);
+	}
+	cout << ans << endl;
+	return 0;
 }
